bc6d7ee5ef009633ebfb809c4a1d010b3d89bde1
hayashihideo <cheedaf@gmail.com>
2018-09-27 17:22:45 +0900

    test for BottomAppBar

--- app/src/main/java/com/hayashihideo/himegoto/HomeActivity.kt
+++ app/src/main/java/com/hayashihideo/himegoto/HomeActivity.kt
@@ -2,11 +2,19 @@ package com.hayashihideo.himegoto
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(appbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return true
     }
 }
